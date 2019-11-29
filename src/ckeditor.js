1dfa6477b37666375bbfbec85c3ab4388ed50117
Laurent Jouanneau <dev@ljouanneau.com>
2019-11-29 12:21:11 +0100

    Enable SimpleUploadAdapter

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -35,6 +35,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -42,6 +43,7 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
+	SimpleUploadAdapter,
 	Autoformat,
 	Alignment,
 	Bold,
